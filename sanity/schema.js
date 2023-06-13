b870acc716b60e6b3483c414292c70f61d54ece6
Richard Stephen <102833845+csr000@users.noreply.github.com>
2023-06-13 15:02:53 +0100

    feat: change the whole frontend & sanity account

---
+++ sanity/schema.js
@@ -0,0 +1,21 @@
+import works from "./schemas/works";
+import testimonials from "./schemas/testimonials";
+import brands from "./schemas/brands";
+import abouts from "./schemas/abouts";
+import experiences from "./schemas/experiences";
+import skills from "./schemas/skills";
+import workExperience from "./schemas/workExperience";
+import contact from "./schemas/contact";
+
+export const schema = {
+  types: [
+    works,
+    testimonials,
+    brands,
+    abouts,
+    skills,
+    workExperience,
+    experiences,
+    contact,
+  ],
+};
